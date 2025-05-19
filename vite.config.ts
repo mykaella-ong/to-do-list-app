eaf149019d6bc607c53a4d1dacce6b3782f1d5fb
Mykaella <mao00272@students.stir.ac.uk>
2025-05-19 17:28:39 +0400

    Fix MIME type issues and add client-side routing support

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        format: 'es'
       }
     }
   }
