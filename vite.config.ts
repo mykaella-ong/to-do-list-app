8066803485ae995df48e8d4e664dc0931402eb7f
Mykaella <mao00272@students.stir.ac.uk>
2025-05-19 18:40:43 +0400

    Add homepage field and update base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? pkg.homepage : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
