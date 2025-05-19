446a584b9ce254285298c443d37609911b762173
Mykaella <mao00272@students.stir.ac.uk>
2025-05-19 17:55:15 +0400

    Switch to Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/to-do-list-app/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
